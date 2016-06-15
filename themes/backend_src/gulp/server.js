e54589dc998a4e4df124e2038d3148a39a3a4a9d
kamal <kamal@hismith.ru>
2016-06-15 20:13:21 +0300

    Spinners position

--- themes/backend_src/gulp/server.js
+++ themes/backend_src/gulp/server.js
@@ -35,7 +35,7 @@ function browserSyncInit(baseDir, browser) {
    */
   server.middleware = proxyMiddleware(['/user','/atom','/transaction','/page','/service','/partner','/role','/article','/cardtype', '/affiliation', '/industry', '/setting', '/accrual', '/writeoff', '/refund', '/page', '/block', '/filemanager', '/thumbs', '/upload', '/log', '/company', '/region','/status', '/contact', '/task', '/properties', '/download', '/join', '/staff', '/statisticsbybrand'], {
   // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', proxyHost: 'jsonplaceholder.typicode.com'});
-    target: 'http://rtaharvest.dev.hismith.ru',
+    target: 'http://bonus.dev.hismith.ru',
     changeOrigin: true
   });
 
