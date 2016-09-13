33639bbfbb44c6f2cb96d7cea37210c36ea79261
masiv <masiv@hismith.ru>
2016-09-13 17:06:36 +0400

    fixed T3601

--- themes/backend_src/src/app/components/buildFormMarkup/markupFactory/HtmlMarkup.js
+++ themes/backend_src/src/app/components/buildFormMarkup/markupFactory/HtmlMarkup.js
@@ -3,9 +3,12 @@ function HtmlMarkup(data) {
   var readonly = (typeof data.readonly !== "undefined" && data.readonly == true)?' readonly ':' ';
     return {
         markup: '<label for="textarea-'+data.name+'" class="col-sm-2">'+data.title+(data.require ? '<span class="require" style="padding-left: 3px;">*</span>' : '')+'</label>'+
-                '<div class="col-sm-10">'+
-                    '<textarea ' +disabled+ readonly+ ' id="textarea-'+data.name+'" data-ui-tinymce class="form-control" rows="4" data-ng-model="form.'+data.name+'"></textarea>'+
-                '</div>',
+                // '<div class="col-sm-10">'+
+                //     '<textarea ' +disabled+ readonly+ ' id="textarea-'+data.name+'" data-ui-tinymce class="form-control" rows="4" data-ng-model="form.'+data.name+'"></textarea>'+
+                // '</div>' +
+        '<div class="col-sm-10">'+
+        '<textarea ' +disabled+ readonly+ ' id="textarea-'+data.name+'" data-froala class="form-control" rows="4" data-ng-model="form.'+data.name+'"></textarea>'+
+        '</div>',
 
         callback: function(wrapper) {
             return true;
--- themes/backend_src/src/app/index.config.js
+++ themes/backend_src/src/app/index.config.js
@@ -1,71 +1,88 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('atom')
-    .config(function($logProvider, $httpProvider, $stateProvider, toastr, SessionServiceProvider) {
-      $httpProvider.defaults.headers.post = {'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'};
-      $httpProvider.defaults.headers.get = {'Accept': 'application/json'};
+    angular.module('atom')
+        .config(function ($logProvider, $httpProvider, $stateProvider, toastr, SessionServiceProvider) {
+            $httpProvider.defaults.headers.post = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            };
+            $httpProvider.defaults.headers.get = {'Accept': 'application/json'};
 
-      if(SessionServiceProvider.$get().getSessionID()) {
-        $httpProvider.defaults.headers.common.Token = SessionServiceProvider.$get().getSessionID();
-      }
-
-      $httpProvider.interceptors.push(function($q, $injector, _, loginPeriod) {
-        return {
-          request: function(config) {
-            var remember = $injector.get('$cookies').get('rememberMe');
-            var token = $injector.get('$cookies').get('token');
-            var homepage = $injector.get('$cookies').get('homepage');
-
-            if(token && remember === '0') {
-              var loginExpirationDate = new Date();
-              loginExpirationDate.setTime( loginExpirationDate.getTime() + loginPeriod );
-              var tokenCookieParams = {
-                expires: loginExpirationDate,
-                path: '/'
-              };
-              $injector.get('$cookies').put('token', token, tokenCookieParams);
-              $injector.get('$cookies').put('rememberMe', token, tokenCookieParams);
-              $injector.get('$cookies').put('homepage', homepage, tokenCookieParams);
+            if (SessionServiceProvider.$get().getSessionID()) {
+                $httpProvider.defaults.headers.common.Token = SessionServiceProvider.$get().getSessionID();
             }
 
-            return config;
-          },
-          responseError:  function(rejection) {
-            if(rejection.status == 500){
-              $injector.get('$rootScope').errorMessage = rejection.data.error.message;
-              $injector.get('$state').go('error500');
-            }
-            if(rejection.status == 401 && rejection.config.url !== "/user/logout/") {
-                $injector.get('$state').go('logout');
-            } else if(rejection.status == 400) {
-              if(rejection.data.error.message)
-                toastr.error(rejection.data.error.message);
-            } else if(rejection.status == 403) {
-              if(rejection.config.url == "/atom/properties/") {
-                if(rejection.data.error.message)
-                  toastr.error(rejection.data.error.message);
-                $injector.get('$state').go('logout');
-              }
-            } else {
-              _.each(rejection.data.error.null, function(error) {
-                if(error.message)
-                  toastr.error(error.message);
-              });
-            }
+            $httpProvider.interceptors.push(function ($q, $injector, _, loginPeriod) {
+                return {
+                    request: function (config) {
+                        var remember = $injector.get('$cookies').get('rememberMe');
+                        var token = $injector.get('$cookies').get('token');
+                        var homepage = $injector.get('$cookies').get('homepage');
 
-            return $q.reject(rejection);
-          }
-        };
-      });
+                        if (token && remember === '0') {
+                            var loginExpirationDate = new Date();
+                            loginExpirationDate.setTime(loginExpirationDate.getTime() + loginPeriod);
+                            var tokenCookieParams = {
+                                expires: loginExpirationDate,
+                                path: '/'
+                            };
+                            $injector.get('$cookies').put('token', token, tokenCookieParams);
+                            $injector.get('$cookies').put('rememberMe', token, tokenCookieParams);
+                            $injector.get('$cookies').put('homepage', homepage, tokenCookieParams);
+                        }
 
-      // Enable log
-      $logProvider.debugEnabled(true);
+                        return config;
+                    },
+                    responseError: function (rejection) {
+                        if (rejection.status == 500) {
+                            $injector.get('$rootScope').errorMessage = rejection.data.error.message;
+                            $injector.get('$state').go('error500');
+                        }
+                        if (rejection.status == 401 && rejection.config.url !== "/user/logout/") {
+                            $injector.get('$state').go('logout');
+                        } else if (rejection.status == 400) {
+                            if (rejection.data.error.message)
+                                toastr.error(rejection.data.error.message);
+                        } else if (rejection.status == 403) {
+                            if (rejection.config.url == "/atom/properties/") {
+                                if (rejection.data.error.message)
+                                    toastr.error(rejection.data.error.message);
+                                $injector.get('$state').go('logout');
+                            }
+                        } else {
+                            _.each(rejection.data.error.null, function (error) {
+                                if (error.message)
+                                    toastr.error(error.message);
+                            });
+                        }
 
-      // Set options third-party lib
-      toastr.options.timeOut = 3000;
-      toastr.options.positionClass = 'toast-top-right';
-      toastr.options.preventDuplicates = false;
-      toastr.options.closeButton = true;
-    });
+                        return $q.reject(rejection);
+                    }
+                };
+            });
+
+            // Enable log
+            $logProvider.debugEnabled(true);
+
+            // Set options third-party lib
+            toastr.options.timeOut = 3000;
+            toastr.options.positionClass = 'toast-top-right';
+            toastr.options.preventDuplicates = false;
+            toastr.options.closeButton = true;
+        })
+        .value('froalaConfig', {
+            toolbarButtons: [
+                'fullscreen', 'bold', 'italic', 'underline', 'strikeThrough',
+                'subscript', 'superscript', 'fontFamily', 'fontSize', '|', 'color',
+                'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat',
+                'align', 'formatOL', 'formatUL', 'outdent', 'indent', '-',
+                'insertLink', 'insertImage', 'insertVideo', 'insertFile',
+                'insertTable', 'undo', 'redo', 'clearFormatting',
+                'selectAll', 'html', 'quote', 'insertHR'],
+            toolbarInline: false,
+            language: 'ru',
+            heightMin: 100,
+            heightMax: 200
+        });
 })();
